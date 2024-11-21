49db9323500e4d937273935c0300f39a4985d3dc
Idrissa Ganemtore <dev@ganemtore.net>
2024-11-21 18:48:06 -0500

    update

--- src/main/java/de/lenneflow/dummyfunctionsleep/controller/FunctionController.java
+++ src/main/java/de/lenneflow/dummyfunctionsleep/controller/FunctionController.java
@@ -6,17 +6,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/dummy")
+@RequestMapping("/api/dummy/sleep")
 @EnableAsync
 public class FunctionController {
 
@@ -29,7 +26,12 @@ public class FunctionController {
     }
 
 
-    @PostMapping("/sleep")
+    @GetMapping("/ping")
+    public String ping(){
+        return "Dummy Function Sleep is working fine";
+    }
+    
+    @PostMapping("/start")
     @Async
     public void sleep(@RequestBody FunctionPayload functionPayload){
 
